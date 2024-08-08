bc4031b1ab74311d2247a9940f39c565d3fbdfe2
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 <valiyevmuiz0407@gmail.com>
2024-08-08 23:42:55 +0500

    add reset password

--- safar_fe/urls.py
+++ safar_fe/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ru/', include('ru.urls')),
-    path('account/', include('account.urls'))
+    path('account/', include('account.urls')),
+    path('', RedirectView.as_view(url='/ru/', permanent=True))
 ]
 
 if settings.DEBUG:
